21359000f9692cc004ad36ae3db8850e7857a05b
Watcharee Kasiban <w@kasiban.com>
2022-04-04 15:05:08 +0200

    react project

---
+++ README.md
@@ -0,0 +1,17 @@
+# Dark/Light Mode
+
+## Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).\
+This project is part of exercise in [Scrimba Learn React](https://scrimba.com/learn/learnreact).
+
+## Purpose of this project
+
+To learn using React-props for styling components
+
+## Screenshot
+#### Light Mode
+![Light mode](https://i.ibb.co/8jTZJ4k/A08-D6124-927-B-4-AB1-B84-A-E0-E20-D8-A6699-1-201-a.jpg)
+
+### Dark Mode
+![Dark mode](https://i.ibb.co/HGJdsg6/8-A032291-51-AF-4-A3-E-85-E5-E94-CEAEF6-FA2-1-201-a.jpg)
---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from "../images/react-icon-small.jpeg"
+
+export default function Navbar(props) {
+    return (
+        <nav
+            className={props.darkMode ? "dark" : ""}
+        >
+            <img src={logo} alt="react-logo" className="nav-logo-icon"/>
+            <h3 className="nav-logo-text">ReactFacts</h3>
+
+            <div className="toggler">
+                <p className="toggler-light">Light</p>
+                <div
+                    className="toggler-slider"
+                    onClick={props.toggleDarkMode}
+                >
+                    <div className="toggler-slider-circle"></div>
+                </div>
+                <p className="toggler-dark">Dark</p>
+            </div>
+        </nav>
+    )
+}
